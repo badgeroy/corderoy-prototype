b78d589d6699608408faee854bc36a5bf04316e9
Reno Raksi <adriano.raksi@gmail.com>
2021-11-07 22:46:11 -0600

    Added react-router for displaying clothing categories

--- src/pages/Wardrobe.js
+++ src/pages/Wardrobe.js
@@ -1,42 +1,58 @@
 import '../styles/Wardrobe.css';
 import React from "react";
-import {Col, Container, Nav, Navbar, NavLink, Row} from "react-bootstrap";
+import {Container, Nav, Navbar, NavLink} from "react-bootstrap";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Catalog from "../components/Catalog";
+
+
+const categories = {
+  "jackets": "Jackets",
+  "coats": "Coats",
+  "jeans": "Jeans",
+  "pants": "Pants & Leggings",
+  "shorts": "Shorts",
+  "skirts": "Skirts",
+  "sweaters-knits": "Sweaters & Knits",
+  "tops": "Tops",
+  "boots": "Boots",
+  "flats": "Flats",
+  "mules": "Mules & Slides",
+  "pumps": "Pumps",
+  "sandals": "Sandals",
+  "sneakers": "Sneakers",
+  "backpacks": "Backpacks",
+  "clutches": "Clutches",
+  "cross": "Cross Body Bags",
+  "satchels": "Satchels & Top Handles",
+  "shoulders": "Shoulder Bags",
+  "totes": "Totes",
+  "hats": "Hats",
+  "scarves": "Scarves & Wraps",
+  "sunglasses": "Sunglasses & Eyewear",
+};
 
 
 class Wardrobe extends React.Component {
     render() {
         return (
           <div className="Wardrobe">
-            <Navbar>
-              <Container fluid className="navbar-container">
-                <Navbar.Brand href="#home">Corderoy</Navbar.Brand>
-                <Nav>
-                  <NavLink>Jackets</NavLink>
-                  <NavLink>Coats</NavLink>
-                  <NavLink>Jeans</NavLink>
-                  <NavLink>Pants & Leggings</NavLink>
-                  <NavLink>Shorts</NavLink>
-                  <NavLink>Skirts</NavLink>
-                  <NavLink>Sweaters & Knits</NavLink>
-                  <NavLink>Tops</NavLink>
-                  <NavLink>Boots</NavLink>
-                  <NavLink>Flats</NavLink>
-                  <NavLink>Mules & Slides</NavLink>
-                  <NavLink>Pumps</NavLink>
-                  <NavLink>Sandals</NavLink>
-                  <NavLink>Sneakers</NavLink>
-                  <NavLink>Backpacks</NavLink>
-                  <NavLink>Clutches</NavLink>
-                  <NavLink>Cross Body Bags</NavLink>
-                  <NavLink>Satchels & Top Handles</NavLink>
-                  <NavLink>Shoulder Bags</NavLink>
-                  <NavLink>Totes</NavLink>
-                  <NavLink>Hats</NavLink>
-                  <NavLink>Scarves & Wraps</NavLink>
-                  <NavLink>Sunglasses & Eyewear</NavLink>
-                </Nav>
-              </Container>
-            </Navbar>
+            <BrowserRouter>
+              <Navbar>
+                <Container fluid className="navbar-container">
+                  <Navbar.Brand href="/">Corderoy</Navbar.Brand>
+                  <Nav>
+                    {Object.entries(categories).map(([idName, dispName]) => (
+                      <NavLink as={Link} to={"/" + idName}>{dispName}</NavLink>
+                    ))}
+                  </Nav>
+                </Container>
+              </Navbar>
+              <Routes>
+                {Object.entries(categories).map(([idName, dispName]) => (
+                    <Route path={"/" + idName} element={<Catalog category={idName}/>}/>
+                ))}
+              </Routes>
+            </BrowserRouter>
           </div>
         );
     }
