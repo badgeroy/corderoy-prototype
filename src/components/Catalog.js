95f0c829df91cd106125ee8c3e92ec20e96b6e34
redrossa <adriano.raksi@gmail.com>
2021-11-11 14:13:48 -0600

    Implemented grids on cards

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,21 +1,32 @@
+import "../styles/Catalog.css"
 import React from "react";
-import {Card} from "react-bootstrap";
+import {Card, Col, Container, Row} from "react-bootstrap";
 
 class Catalog extends React.Component {
     render() {
+        const products = this.props.category.getProducts();
         return (
             <div className="Catalog">
               <div className="filter-menu"></div>
               <div className="cards">
-                {this.props.category.getProducts().map(prod => (
-                    <Card style={{ width: '18rem' }}>
-                      <Card.Img variant="top" src={"https://m.media-amazon.com/images/G/01/Shopbop/p" + prod.getMainImage()} />
-                      <Card.Body>
-                        <Card.Title>{prod.getDesigner()}</Card.Title>
-                        <Card.Text>{prod.getDescription()}</Card.Text>
-                      </Card.Body>
-                    </Card>
-                ))}
+                <Container fluid>
+                  <Row xs={5} className="g-4">
+                      {this.props.category.getProducts().map(prod => (
+                          <Col>
+                            <Card style={{ width: '12rem' }}>
+                              <Card.Img variant="top" src={"https://m.media-amazon.com/images/G/01/Shopbop/p" + prod.getMainImage()} />
+                              <Card.Body>
+                                <Card.Title>{prod.getDesigner()}</Card.Title>
+                                <div className="card-label">
+                                  <Card.Text className="card-desc">{prod.getDescription()}</Card.Text>
+                                  <Card.Text className="card-price">{prod.getPrice()}</Card.Text>
+                                </div>
+                              </Card.Body>
+                            </Card>
+                          </Col>
+                      ))}
+                  </Row>
+                </Container>
               </div>
             </div>
         );
